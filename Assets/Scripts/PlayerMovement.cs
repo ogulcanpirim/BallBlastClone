b7a8a225eb3ab92bada6c2ec004f64a6340c28c5
ogulcanpirim <opirim@gmail.com>
2021-06-05 16:17:41 +0300

    Player Die

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -43,12 +44,14 @@ public class PlayerMovement : MonoBehaviour
             nextBulletTime += bulletPeriod;
         }
         transform.position = pos;
-
+        
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("vuruldum !");
+        //freeze scene
+        Time.timeScale = 0;
+        
     }
 }
